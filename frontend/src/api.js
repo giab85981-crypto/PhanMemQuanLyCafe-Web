02eebfc3ed830eb8c3b2deaf5317e5d3752f3cb5
Châu Gia Bảo <giab85981@gmail.com>
2026-09-08 21:10:57 +0700

    Mô tả bạn vừa làm gì

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5142/api',   // ✅ đúng không được sửa dòng này
+  baseURL: 'https://localhost:7196/api',   // ✅ đúng
 })
 
 // Interceptor: tự động gắn "Authorization: Bearer <token>" nếu đã đăng nhập
